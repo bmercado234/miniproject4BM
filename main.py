1b3ab70d0bd1b779ff1b7e6650383f40ee7c70e1
Braulio Mercado <b_mercado@mail.fhsu.edu>
2023-10-30 19:27:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# INF601 - Advanced Programming in Python
+# Braulio Mercado
+# Mini Project 3
+# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
